85d0d5a16db8174c7bfdb7bcb91bdda77aad68ea
Govind <42005543+codingfusion@users.noreply.github.com>
2020-02-09 20:09:48 +0530

    Created simple java file

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello Java");
+    System.out.println("Java is actually very easy,if we focus hard..");
+    }
+    }
