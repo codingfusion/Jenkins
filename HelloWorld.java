c959b51414d86cb838f7d4f9c49bdad72f2d5f56
Govind <42005543+codingfusion@users.noreply.github.com>
2020-02-11 20:48:33 +0530

    Testing jobs in Jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
   public static void main(String[] args){
-    System.out.println("Hello Java..");
-    System.out.println("Jenkins test");
+    System.out.println("Hello Java");
+    System.out.println("Jenkins test in progress..");
     }
  }
