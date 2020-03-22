791f78c13efbc88b8e015376c2a0be380ecafae5
codingfusion <govindbhagat@hotmail.com>
2020-03-22 06:05:08 +0000

    Simple text check build 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello Java");
-    System.out.println("Jenkins test in progress...");
+    System.out.println("Jenkins test in progress..");
     System.out.println("You have to treat your mental disorders SOON")
     }
  }
