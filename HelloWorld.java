db49c5902b55318f9a485299f947d788463cfa3f
codingfusion <govindbhagat@hotmail.com>
2020-03-22 05:47:32 +0000

    commit from console of ec2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello Java");
     System.out.println("Jenkins test in progress...");
-    System.out.println("You have to treat your mental disorders SOON.")
+    System.out.println("You have to treat your mental disorders SOON..")
     }
  }
